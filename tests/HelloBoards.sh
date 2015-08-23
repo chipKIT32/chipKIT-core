75c58c2978412c7b57bda44d4f8004715dd568f9
ricklon <rick.rickanderson@gmail.com>
2015-08-22 17:00:38 -0700

    Issue #43 example tests added. Issue #44 comment out chipkit_WiFire_80MHz

---
+++ tests/HelloBoards.sh
@@ -0,0 +1,72 @@
+echo "HelloTest Boards"
+echo "cerebot32mx4"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:cerebot32mx4 --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "cerebot32mx7"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:cerebot32mx7 --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "cerebot_mx3ck"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:cerebot_mx3ck --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "chipkit_mx3"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:chipkit_mx3 --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "cerebot_mx3ck_512"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:cerebot_mx3ck_512 --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "cerebot_mx4ck"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:cerebot_mx4ck --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "chipkit_pro_mx4"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:chipkit_pro_mx4 --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "cerebot_mx7ck"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:cerebot_mx7ck --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "chipkit_pro_mx7"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:chipkit_pro_mx7 --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "chipkit_Pi"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:chipkit_Pi --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "chipkit_Pi_USB_Serial"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:chipkit_Pi_USB_Serial --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "cmod"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:cmod --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "CUI32stem"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:CUI32stem --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "ubw32_mx460"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:ubw32_mx460 --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "ubw32_mx795"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:ubw32_mx795 --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "cui32"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:cui32 --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "usbono_pic32"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:usbono_pic32 --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "chipkit_DP32"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:chipkit_DP32 --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "fubarino_mini_dev"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:fubarino_mini_dev --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "fubarino_mini"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:fubarino_mini --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "fubarino_sd_seeed"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:fubarino_sd_seeed --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "fubarino_sd"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:fubarino_sd --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "Fubarino_SDZ"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:Fubarino_SDZ --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "mega_pic32"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:mega_pic32 --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "mega_usb_pic32"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:mega_usb_pic32 --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "Olimex_Pinguino32"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:Olimex_Pinguino32 --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "picadillo_35t"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:picadillo_35t --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "quick240_usb_pic32"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:quick240_usb_pic32 --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "chipkit_uc32"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:chipkit_uc32 --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "uc32_pmod"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:uc32_pmod --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "uno_pic32"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:uno_pic32 --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "uno_pmod"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:uno_pmod --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "chipkit_WF32"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:chipkit_WF32 --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "chipkit_WiFire"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:chipkit_WiFire --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+echo "openbci"
+/Applications/Arduino.app/Contents/MacOS/Arduino  --board chipkit-core:pic32:openbci --verify  /Users/rickanderson/projects/chipKIT/chipKIT-core/tests/sketches/TestHello/TestHello.ino
+
