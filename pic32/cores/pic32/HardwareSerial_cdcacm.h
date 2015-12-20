0cbfec22743a44af0890ee9251076bb0dd574ab5
Brian Schmalz <brian@schmalzhaus.com>
2015-12-20 13:54:13 -0600

    Misc. warnings fixed with USB code.

--- pic32/cores/pic32/HardwareSerial_cdcacm.h
+++ pic32/cores/pic32/HardwareSerial_cdcacm.h
@@ -19,7 +19,7 @@ typedef boolean (*cdcacm_storedata_cbfn)(const byte *buffer, int length);
 	extern "C"{
 #endif
 
-	void	cdcacm_print(const unsigned char *line, int length);
+	void	cdcacm_print(const unsigned char *line, unsigned int length);
 	void	cdcacm_command_ack(void);
 	void	cdcacm_register(cdcacm_reset_cbfn reset, cdcacm_storedata_cbfn storeData);  // revisit -- register receive upcall!
 
