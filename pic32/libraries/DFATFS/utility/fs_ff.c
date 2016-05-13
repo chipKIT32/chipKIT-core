9e692e0442210526f2155b5cfda08057b35301b5
Matt Jenkins <matt@majenko.co.uk>
2016-05-13 22:36:09 +0100

    Added LFN support to DFATFS

--- pic32/libraries/DFATFS/utility/fs_ff.c
+++ pic32/libraries/DFATFS/utility/fs_ff.c
@@ -554,6 +554,30 @@ static const BYTE ExCvt[] = _EXCVT;	/* Upper conversion table for extended chara
 
 ---------------------------------------------------------------------------*/
 
+WCHAR ff_convert (WCHAR wch, UINT dir)
+{
+    if (wch < 0x80) {
+        /* ASCII Char */
+        return wch;
+    }
+
+    /* I don't support unicode it is too big! */
+    return 0;
+}
+
+WCHAR ff_wtoupper (WCHAR wch)
+{
+    if (wch < 0x80) {
+        /* ASCII Char */
+        if (wch >= 'a' && wch <= 'z') {
+            wch &= ~0x20;
+        }
+        return wch;
+    }
+
+    /* I don't support unicode it is too big! */
+    return 0;
+}
 
 /*-----------------------------------------------------------------------*/
 /* String functions                                                      */
