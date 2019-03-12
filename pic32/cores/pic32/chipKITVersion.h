e9dad877978fa3edb9b72b83197198b5e9de66e8
Wilheml Wiens <wilhelmwiens@gmail.com>
2019-03-12 20:49:01 +0100

    revert change on version file

--- pic32/cores/pic32/chipKITVersion.h
+++ pic32/cores/pic32/chipKITVersion.h
@@ -1,7 +1,7 @@
 #ifndef _CHIPKIT_VERSION
 #define _CHIPKIT_VERSION
 
-// Version cdcacm_sendWithoutBlockCopy-v2.0.6-6-g66e01ad
-#define __CHIPKIT__ 20006
+// Version master-v2.0.5
+#define __CHIPKIT__ 20005
 
 #endif
